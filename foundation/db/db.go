02ddad2632d1b02d05cbf51fd23e54e946a7faee
Hank <853160447@qq.com>
2020-01-03 09:01:05 +0800

    xorm templates

--- foundation/db/db.go
+++ foundation/db/db.go
@@ -1,7 +1,10 @@
 package db
 
 import(
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
+	"github.com/xormplus/core"
 	"github.com/xormplus/xorm"
 )
 
@@ -11,5 +14,23 @@ var(
 
 func Init(){
 	logs.Debug("|foundation|init|db|Init")
+	var(
+		err error
+	)
 
+	dbSource := beego.AppConfig.String("dbsource")
+	if mallEngine,err = xorm.NewEngine("mysql",dbSource);err != nil{
+		logs.Error("Engine Init Err:%v", err)
+		panic(err)
+	}
+
+	mallEngine.ShowSQL(true)
+	mallEngine.ShowExecTime(true)
+	mallEngine.SetLogger(xorm.NewSimpleLogger(beego.BeeLogger))
+	mallEngine.Logger().SetLevel(core.LOG_INFO)
+}
+
+//Engine -主数据库
+func Engine()*xorm.Engine{
+	return mallEngine
 }
