a81cfd84e3f260150f7520da21a8cf39f852f976
Hank <853160447@qq.com>
2020-01-07 08:09:44 +0800

    db

--- foundation/foundation.go
+++ foundation/foundation.go
@@ -1,8 +1,13 @@
 package foundation
 
-import "github.com/friendlyhank/goredis"
+import (
+	"github.com/friendlyhank/goredis"
+	"github.com/friendlyhank/multicache/foundation/db"
+)
 
 func init(){
+	//数据库初始化
+	db.Init()
 	//redis初始化
 	rds.Init()
 }
